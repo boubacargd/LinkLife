2b8ce72c95b30c76d72d9d6afc77e01dcb90408a
boubacargd <dev.boubacargadio@gmail.com>
2024-05-08 01:47:21 +0200

    backEnd

---
+++ backEnd_app/src/main/java/org/example/backend_app/BackEndAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.backend_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndAppApplication.class, args);
+    }
+
+}
