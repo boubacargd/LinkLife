e39a4d487d7e4b64e0de2628181230692e972ad2
boubacargd <dev.boubacargadio@gmail.com>
2024-05-08 02:29:52 +0200

    install: spring & angular

--- backEnd_app/src/main/java/org/example/backend_app/BackEndAppApplication.java
+++ backEnd_app/src/main/java/org/example/backend_app/BackEndAppApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackEndAppApplication {
 
     public static void main(String[] args) {
+        System.out.printf("hello world");
         SpringApplication.run(BackEndAppApplication.class, args);
     }
 
